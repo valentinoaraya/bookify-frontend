11db2c3eb6b76ed6334da16de16e082841a21aca
valentinoaraya <valentinoaraya04@gmail.com>
2024-09-04 18:11:07 -0300

    Primer commit - Creo la págima Home

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from './components/Home/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className='mainPage'>
+        <Home />
+      </main>
+    </>
+  )
+}
+
+export default App
